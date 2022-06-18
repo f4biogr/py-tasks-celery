67ea236d29b53355da44141289207997a95e48c5
Fábio da Rosa <fabio.gorosa@gmail.com>
2022-06-17 23:05:16 -0300

    dependencies update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 from tasks import salve_do_fabio
 
+
 salve_do_fabio.delay()
--- tasks.py
+++ tasks.py
@@ -1,15 +1,14 @@
 from celery import Celery
 
-app = Celery('tasks', broker='redis://localhost:6379/0')
+
+app = Celery("tasks", broker="redis://localhost:6379/0")
+
 
 @app.task
-
 def salve_do_fabio():
     return "Um salve do Fábio!"
 
+
 app.conf.beat_schedule = {
-    "run-every-ten-seconds": {
-        "task": "tasks.salve_do_fabio",
-        "schedule": 10.0
-    }
+    "run-every-ten-seconds": {"task": "tasks.salve_do_fabio", "schedule": 10.0}
 }
